219aca72466de440c02740d6b572feeec0a93491
Jing Li <lijing900913@gmail.com>
2022-01-04 23:05:38 -0500

    Install uuid and add routes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -81,7 +81,6 @@ const handleNoteSave = () => {
 const handleNoteDelete = (e) => {
   // Prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
-
   const note = e.target;
   const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const db = require('./db/db.json');
 
 const PORT = process.env.port || 3001;
@@ -30,14 +30,38 @@ app.get('/api/notes', (req, res) =>
 );
 
 // POST Route for a new note
-app.post('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
+app.post('/api/notes', (req, res) =>{
+  if(req.body) {
+    const { title, text } = req.body;
+    const newNote = {
+      title,
+      text, 
+      id: uuidv4() 
+    };
+    // newNote.id = uuid.v4();
+    let notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    // console.log(notes);
+    notes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json('New note has been added!');
+  } else {
+    res.error(`Can't add the new note in db!`);
+  }
+});
 
 // DELETE Route for a existing note
-app.delete('/api/notes/:id', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
+app.delete('/api/notes/:id', (req, res) =>{
+  const id = req.params.id;
+  let notes = JSON.parse(fs.readFileSync('./db/db.json'));
+  for (let i=0; i<notes.length; i++){
+    if (notes[i].id === id){
+      notes.splice(i, 1);
+    } 
+  }
+  console.log(notes);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json('Item has been deleted!');
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
